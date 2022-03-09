951f9b1a20bf0bdc8b645afcc98fc27cbfb556fa
Mikkel Wied Frederiksen <mikkel@wied.cc>
2022-03-09 10:21:47 +0100

    Better container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
+    screens: {
+      sm: "768px",
+      md: "1024px",
+      lg: "1440px",
+    },
+    container: {
+      padding: "1rem",
+    },
     fontFamily: {
       body: ["Akkurat Pro", "Tahoma", "sans-serif"],
       header: ["Gotham", "sans-serif"],
