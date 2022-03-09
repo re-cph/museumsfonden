9de271976e294c529a752722dc450651586b21a2
Mikkel Wied Frederiksen <mikkel@wied.cc>
2022-03-09 09:32:36 +0100

    Install fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      body: ["Akkurat Pro", "Tahoma", "sans-serif"],
+      header: ["Gotham", "sans-serif"],
+    },
+  },
 };
