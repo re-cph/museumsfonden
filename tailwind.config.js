c34471d18f56e68737b19508236888f55b001b77
Mikkel Wied Frederiksen <mikkel@wied.cc>
2022-03-09 10:37:55 +0100

    Defined font-sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
       body: ["Akkurat Pro", "Tahoma", "sans-serif"],
       header: ["Gotham", "sans-serif"],
     },
+    fontSize: {
+      menu: ["15px", "1.5em"],
+      body: ["18px", "1.5em"],
+      header: ["24px", "1.5em"],
+      title: ["20px", "1.5em"],
+    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
